63b38f2f0a7fcae860a9a63634917c363283b44b
asmanya <ishuisu147@gmail.com>
2023-09-07 14:56:33 +0530

    newUpdate

---
+++ DSA Course/strings/caseChange.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char s[] = "wElcOmE";
+    int i;
+
+    // UPPER CASE TO LOWER CASE
+    /* for (i = 0; s[i] != '\0'; i++)
+    {
+        s[i] += 32;
+    }
+    cout << s; */
+
+    // TOGGLE THE CASES
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] >= 65 && s[i] <= 90)
+        {
+            s[i] += 32;
+        }
+        else if (s[i] >= 97 && s[i] <= 122)
+        {
+            s[i] -= 32;
+        }
+    }
+    cout << s;
+
+    return 0;
+}
