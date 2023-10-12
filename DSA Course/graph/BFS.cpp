4275a5b87e2547da7bf4dc6e3cca87e074a72d80
asmanya <ishuisu147@gmail.com>
2023-10-12 18:04:29 +0530

    graph

---
+++ DSA Course/graph/BFS.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void BFS(int G[][7], int start, int n)
+{
+    int i = start;
+}
+
+int main()
+{
+    int G[7][7] = {
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 1, 1, 0, 0, 0},
+        {0, 1, 0, 0, 1, 0, 0},
+        {0, 1, 0, 0, 1, 0, 0},
+        {0, 0, 1, 1, 0, 1, 1},
+        {0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0},
+    };
+    return 0;
+}
